ba56d3f8edb7ae051f1c6429b4ab73ef9aa30dca
StephanZuniga <szaleman14@gmail.com>
2020-03-20 00:19:46 -0600

    avances de Main

--- src/edu/ujcv/progra1/Main.java
+++ src/edu/ujcv/progra1/Main.java
@@ -1,19 +1,27 @@
 package edu.ujcv.progra1;
-
+import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
 
-        int[] elementos = {3,2,4,5,6,7,9,9,10,3,5};
-        ShuffleSort s = new ShuffleSort();
-        elementos = s.shuffleSort(elementos);
-
-        for (int i = 0; i < elementos.length; i++) {
-            System.out.println(elementos[i]);
+        int[] array = new int [10000];
+        Random R = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = Math.abs(R.nextInt(100000));
         }
 
-        elementos = new int[]{3,2,4,5,6,7,9,9,10,3,5};
-        System.out.println("\n\ntarda "+ s.sort(elementos)+ " milisegundos");
+        //HeapSort h= HeapSort();
+        //h.heapSort(array);
+        //MergeSort m = new MergeSort();
+        //array = m.mergeSort(array);
+        //BubleSort b = new BubleSort();
+        //array = b.bubleSort(array);
+        RecursiveInsertionSort ri = new RecursiveInsertionSort();
+        ri.insertionSortRecursive(array, array.length);
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+
+        System.out.println("\n\ntarda "+ s.sort()+ " milisegundos");
     }
 }
